4d093565e4e97b81852df420d7ada28f282cbeed
Rameez071 <rameezraja405@gmail.com>
2020-09-30 01:02:50 +0530

    Add : UserName validation code added

---
+++ userRegistration.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -x
+
+#constants
+VALIDNAMEPATTERN="^[A-Z]{1}[a-z]{2,}$"
+
+#function for pattern check
+function patternCheck()
+{
+   if [[ $1 =~ $2 ]]
+   then
+      echo "Valid Pattern."
+   else
+      echo "Invalid Pattern."
+   fi
+}
+
+#Function calling and passing parameter
+read -p "Enter first name: " firstName
+patternCheck $firstName $VALIDNAMEPATTERN
